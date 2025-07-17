71e2279b00309a12850eb62bc27880dcc9ed5976
selasikwame2006dti <richmond.agbenyefia@dtiafrica.edu.gh>
2025-07-17 14:20:42 +0000

    Update script.js

--- Artchive/script.js
+++ Artchive/script.js
@@ -26,12 +26,12 @@
             // --- Initial Setup ---
             async function initializeGallery() {
                 try {
-                    const imageCount = 300;
+                    const imageCount = 50;
                     
                     // Using a seeded random ensures images are consistent between reloads.
                     galleryItems = Array.from({ length: imageCount }, (_, i) => ({
                         id: `gallery-image-${i}`,
-                        src: `https://api.unsplash.com/photos${i+1}/1920/1080`, // Using picsum.photos for random images
+                        src: `https://api.unsplash.com/photos`, // Using picsum.photos for random images
                         type: 'image',
                         alt: `random abstract image ${i}`
                     }));
